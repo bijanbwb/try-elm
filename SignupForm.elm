1d87457c00f5875cb17d0e13a1e5fc1f53e8c24c
Bijan <bijanbwb@gmail.com>
2016-06-19 10:51:52 -0400

    Check GitHub API for username availability

--- SignupForm.elm
+++ SignupForm.elm
@@ -5,6 +5,8 @@ import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (id, type', for, value, class)
 import Http
+import Task exposing (Task)
+import Json.Decode exposing (succeed)
 
 view model =
     form [ id "signup-form" ]
@@ -17,7 +19,7 @@ view model =
             , onInput (\str -> { msgType = "SET_USERNAME", payload = str })
             ]
             []
-        , div [ class "validation-error" ] [ text model.errors.username ]
+        , div [ class "validation-error" ] [ text (viewUsernameErrors model) ]
         , label [ for "password" ] [ text "password: " ]
         , input
             [ id "password-field"
@@ -30,12 +32,19 @@ view model =
         , div [ class "signup-button", onClick { msgType = "VALIDATE", payload = "" } ] [ text "Sign Up!" ]
         ]
 
+viewUsernameErrors model =
+   if model.errors.usernameTaken then
+       "That username is taken!"
+   else
+       model.errors.username
+
 getErrors model =
     { username =
         if model.username == "" then
             "Please enter a username!"
         else
             ""
+    , usernameTaken = model.errors.usernameTaken
     , password =
         if model.password == "" then
             "Please enter a password!"
@@ -45,7 +54,27 @@ getErrors model =
 
 update msg model =
     if msg.msgType == "VALIDATE" then
-        ( { model | errors = getErrors model }, Cmd.none )
+        let
+            url =
+                "https://api.github.com/users/" ++ model.username
+
+            failureToMsg err =
+                { msgType = "USERNAME_AVAILABLE", payload = "" }
+
+            successToMsg result =
+                { msgType = "USERNAME_TAKEN", payload = "" }
+
+            request =
+                Http.get (succeed "") url
+
+            cmd =
+                Task.perform failureToMsg successToMsg request
+        in
+            ( { model | errors = getErrors model }, cmd )
+    else if msg.msgType == "USERNAME_TAKEN" then
+        ( withUsernameTaken True model, Cmd.none )
+    else if msg.msgType == "USERNAME_AVAILABLE" then
+        ( withUsernameTaken False model, Cmd.none )
     else if msg.msgType == "SET_USERNAME" then
         ( { model | username = msg.payload }, Cmd.none )
     else if msg.msgType == "SET_PASSWORD" then
@@ -53,8 +82,18 @@ update msg model =
     else
         ( model, Cmd.none )
 
+withUsernameTaken isTaken model =
+    let
+        currentErrors =
+            model.errors
+
+        newErrors =
+            { currentErrors | usernameTaken = isTaken }
+    in
+        { model | errors = newErrors }
+
 initialErrors =
-    { username = "", password = "" }
+    { username = "", password = "", usernameTaken = False }
 
 initialModel =
     { username = "", password = "", errors = initialErrors }
