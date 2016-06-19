a75beb0acfffa1f8d226c27091a26311480b5757
Bijan <bijanbwb@gmail.com>
2016-06-19 10:34:35 -0400

    Import Http, clean up

--- SignupForm.elm
+++ SignupForm.elm
@@ -1,33 +1,10 @@
 module SignupForm exposing (..)
 
--- This is where our Elm logic lives.`module SignupForm` declares that this is
--- the SignupForm module, which is how other modules will reference this one
--- if they want to import it and reuse its code.
-
 import Html.App
-
-
--- Elm’s "import" keyword works similarly to "require" in node.js.
-
 import Html exposing (..)
-
-
--- The “exposing (..)” option says that we want to bring the Html module’s contents
--- into this file’s current namespace, so that instead of writing out
--- Html.form and Html.label we can use "form" and "label" without the "Html."
-
 import Html.Events exposing (..)
-
-
--- This works the same way; we also want to import the entire
--- Html.Events module into the current namespace.
-
 import Html.Attributes exposing (id, type', for, value, class)
-
-
--- With this import we are only bringing a few specific functions into our
--- namespace, specifically "id", "type'", "for", "value", and "class".
-
+import Http
 
 view model =
     form [ id "signup-form" ]
@@ -53,13 +30,6 @@ view model =
         , div [ class "signup-button", onClick { msgType = "VALIDATE", payload = "" } ] [ text "Sign Up!" ]
         ]
 
-
-
--- Take a look at this starting model we’re passing to our view function.
--- Note that in Elm syntax, we use = to separate fields from values
--- instead of : like JavaScript uses for its object literals.
-
-
 getErrors model =
     { username =
         if model.username == "" then
@@ -73,7 +43,6 @@ getErrors model =
             ""
     }
 
-
 update msg model =
     if msg.msgType == "VALIDATE" then
         ( { model | errors = getErrors model }, Cmd.none )
@@ -84,14 +53,11 @@ update msg model =
     else
         ( model, Cmd.none )
 
-
 initialErrors =
     { username = "", password = "" }
 
-
 initialModel =
     { username = "", password = "", errors = initialErrors }
-
 
 main =
     Html.App.program
@@ -100,3 +66,4 @@ main =
         , update = update
         , subscriptions = \_ -> Sub.none
         }
+
